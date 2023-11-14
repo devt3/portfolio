584fac94ebc508501ca816e48a981c6d601da0c6
Tapuwa <tapuwa@tapuwas-air.home>
2023-11-14 12:06:56 +0000

    Inital Navbar changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,12 +2,46 @@ import { Link } from "react-router-dom";
 import Logo from "./assets/initialT.svg";
 import "./Navbar.scss";
 
+import CottageIcon from "@mui/icons-material/Cottage";
+
 function Navbar() {
   return (
     <div className="navbar">
       <Link className="logo" to="/home">
         <img src={Logo} alt="my initials" />
       </Link>
+
+      <nav className="navbar-nav">
+        <ul>
+          <li className="nav-item">
+            <Link className="nav-item" to="/home">
+              Home
+              <CottageIcon />
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-item" to="/home">
+              Profile
+              <CottageIcon />
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-item" to="/home">
+              Projects
+              <CottageIcon />
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-item" to="/home">
+              Contact
+              <CottageIcon />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }

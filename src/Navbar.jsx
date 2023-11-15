8f5711105f1f1cffdee3f1611c07e3910a273bae
Tapuwa <tapuwa@tapuwas-air.home>
2023-11-15 12:27:29 +0000

    Initial Navbar and transition animations finalised

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,48 +1,82 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "./assets/initialT.svg";
 import "./Navbar.scss";
 
-import CottageIcon from "@mui/icons-material/Cottage";
+import CottageTwoToneIcon from "@mui/icons-material/CottageTwoTone";
+import PersonOutlineTwoToneIcon from "@mui/icons-material/PersonOutlineTwoTone";
+import WebhookTwoToneIcon from "@mui/icons-material/WebhookTwoTone";
+import AlternateEmailTwoToneIcon from "@mui/icons-material/AlternateEmailTwoTone";
+import SettingsTwoToneIcon from "@mui/icons-material/SettingsTwoTone";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <Link className="logo" to="/home">
-        <img src={Logo} alt="my initials" />
-      </Link>
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <img className="logo" src={Logo} alt="my initial as logo" />
+        </li>
 
-      <nav className="navbar-nav">
-        <ul>
-          <li className="nav-item">
-            <Link className="nav-item" to="/home">
-              Home
-              <CottageIcon />
-            </Link>
-          </li>
+        <li className="nav-item">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "nav-item-active" : "nav-item-inactive"
+            }
+          >
+            <CottageTwoToneIcon />
+            <span className="nav-item-text">Home</span>
+          </NavLink>
+        </li>
 
-          <li className="nav-item">
-            <Link className="nav-item" to="/home">
-              Profile
-              <CottageIcon />
-            </Link>
-          </li>
+        <li className="nav-item">
+          <NavLink
+            to="/profile"
+            className={({ isActive }) =>
+              isActive ? "nav-item-active" : "nav-item-inactive"
+            }
+          >
+            <PersonOutlineTwoToneIcon />
+            <span className="nav-item-text">Profile</span>
+          </NavLink>
+        </li>
 
-          <li className="nav-item">
-            <Link className="nav-item" to="/home">
-              Projects
-              <CottageIcon />
-            </Link>
-          </li>
+        <li className="nav-item">
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              isActive ? "nav-item-active" : "nav-item-inactive"
+            }
+          >
+            <WebhookTwoToneIcon />
+            <span className="nav-item-text">Projects</span>
+          </NavLink>
+        </li>
 
-          <li className="nav-item">
-            <Link className="nav-item" to="/home">
-              Contact
-              <CottageIcon />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        <li className="nav-item">
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "nav-item-active" : "nav-item-inactive"
+            }
+          >
+            <AlternateEmailTwoToneIcon />
+            <span className="nav-item-text">Contact</span>
+          </NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink
+            to="/home"
+            className={({ isActive }) =>
+              isActive ? "nav-item-active" : "nav-item-inactive"
+            }
+          >
+            <SettingsTwoToneIcon />
+            <span className="nav-item-text">Settings</span>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
